498dd156433103c50ba0229da03fa02dadd12d68
Oscar San <oscarsani007@gmail.com>
2025-01-31 14:05:35 -0500

    Simple Meat Cooking Mechanic Implemented
    - Added scripts to accommodate the food cooking system
    - Implemented cooking code for meat
    - Added tag for cooker on the grill
    - Added an OnTrigger collider for the grill

---
+++ Crash Kitchen/Assets/Scripts/Food/Dish.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Dish : Food
+{
+    public float quality;
+    public bool isBagged;
+}
---
+++ Crash Kitchen/Assets/Scripts/Food/Food.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class Food : MonoBehaviour
+{
+    // bool is Interactable
+    public string foodName;
+}
