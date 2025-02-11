bea7fb75d6597a62ceff063385d7e68251b7e319
Timothy Mah <121048273+Tim-Mah@users.noreply.github.com>
2025-02-10 22:12:53 -0500

    Draw Triangle....Not working yet

--- Crash Kitchen/Assets/GenerationScripts/MeshGenerator.cs
+++ Crash Kitchen/Assets/GenerationScripts/MeshGenerator.cs
@@ -11,5 +11,30 @@ public class MeshGenerator : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
     }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3 (0, 0, 0),
+            new Vector3 (0, 0, 1),
+            new Vector3 (1, 0, 0)
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+        };
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
 }
