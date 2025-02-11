1fbb825c2b4c40dda518883683d479c8f6f08a0f
Timothy Mah <121048273+Tim-Mah@users.noreply.github.com>
2025-02-10 22:01:17 -0500

    Update MeshGenerator.cs

--- Crash Kitchen/Assets/GenerationScripts/MeshGenerator.cs
+++ Crash Kitchen/Assets/GenerationScripts/MeshGenerator.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
     }
 }
