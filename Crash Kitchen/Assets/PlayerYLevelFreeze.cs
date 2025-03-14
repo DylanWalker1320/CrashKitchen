c7a3153e63974ff0f5eb7773f37a7bd54a3c6777
Schuyler Good <schuylergood@gmail.com>
2025-03-14 13:20:55 -0400

    Locked Y position

---
+++ Crash Kitchen/Assets/PlayerYLevelFreeze.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerYLevelFreeze : NetworkBehaviour
+{
+    public bool lockYPosition = false;
+    private CharacterController characterController;
+    private float fixedYPosition = 0f;
+    
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (IsOwner && lockYPosition)
+        {
+            // Get current position
+            Vector3 currentPosition = transform.position;
+            
+            // If Y position has changed, reset it
+            if (currentPosition.y != fixedYPosition)
+            {
+                // Create a new position with the fixed Y value
+                Vector3 fixedPosition = new Vector3(currentPosition.x, fixedYPosition, currentPosition.z);
+                
+                // Move the character controller to the fixed position
+                // We use a zero vector because we just want to reset position, not add movement
+                characterController.enabled = false;
+                transform.position = fixedPosition;
+                characterController.enabled = true;
+            }
+        }
+    }
+}
