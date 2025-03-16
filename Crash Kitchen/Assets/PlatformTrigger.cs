624fa5b827d2e2726c6a663cbf1f5f3aa73d6882
Dylan Walker <dylan.walker1320@gmail.com>
2025-03-16 18:56:24 -0400

    Revert "Update PlatformTrigger.cs"
    This reverts commit 1c74a69ebb89194a47d33396dd9d4cf220e33f3d.

--- Crash Kitchen/Assets/PlatformTrigger.cs
+++ Crash Kitchen/Assets/PlatformTrigger.cs
@@ -1,13 +1,11 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class PlatformTrigger : NetworkBehaviour
+public class PlatformTrigger : MonoBehaviour
 {
     public GameManager.RoleType platformType;
     private GameManager gmInstance;
-    
-    // Replace boolean with NetworkVariable
-    public NetworkVariable<bool> touched = new NetworkVariable<bool>(false);
+    private bool touched = false;
 
     void Start() {
         gmInstance = GameManager.Instance;
@@ -17,34 +15,10 @@ public class PlatformTrigger : NetworkBehaviour
     {
         gmInstance.Log("Player entered platform trigger");
 
-        if (other.CompareTag("Player") && !touched.Value)
+        if (other.CompareTag("Player") && !touched)
         {
-            if (IsServer)
-            {
-                // Server can directly modify the NetworkVariable
-                touched.Value = true;
-                gmInstance.AssignPlayerToTruck(other.gameObject, platformType);
-            }
-            else
-            {
-                // Clients need to request the server to handle this
-                SetTouchedServerRpc(other.GetComponent<NetworkObject>());
-            }
-        }
-    }
-    
-    [ServerRpc(RequireOwnership = false)]
-    private void SetTouchedServerRpc(NetworkObjectReference playerRef)
-    {
-        // Double check to prevent race conditions
-        if (touched.Value)
-            return;
-            
-        touched.Value = true;
-        
-        if (playerRef.TryGet(out NetworkObject playerNetObj))
-        {
-            gmInstance.AssignPlayerToTruck(playerNetObj.gameObject, platformType);
+            touched = true;
+            gmInstance.AssignPlayerToTruck(other.gameObject, platformType);
         }
     }
 }
