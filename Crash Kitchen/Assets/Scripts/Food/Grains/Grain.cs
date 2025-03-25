dd80519a86f43c72869d30c42a6e78040b6d699d
Oscar San <oscarsani007@gmail.com>
2025-03-25 01:42:41 -0400

    changed an issue with bun

--- Crash Kitchen/Assets/Scripts/Food/Grains/Grain.cs
+++ Crash Kitchen/Assets/Scripts/Food/Grains/Grain.cs
@@ -13,7 +13,7 @@ public class Grain : Ingredient
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        foodName = bunType.ToString();
+        name = bunType.ToString();
         gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(0.71f, 0.4f, 0.16f)); //Change colour depending on enum
     }
 
